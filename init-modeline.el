1a0cdcdd3433b3ed1620fed481f93ef62c2dcb6d
zuoshifan <zuoshifan@163.com>
2014-02-15 21:27:07 +0800

    Resume my own color theme and modeline.
    Don't use color-theme-molokai.
    Resume modeline face.

--- init-modeline.el
+++ init-modeline.el
@@ -5,7 +5,7 @@
 (setq-default mode-line-format
   (list
     ;; the buffer name; the file name as a tool tip
-    '(:eval (propertize "%b " 'face nil
+    '(:eval (propertize "%b " 'face 'font-lock-type-face
         'help-echo (buffer-file-name)))
 
     ;; line and column
@@ -18,40 +18,39 @@
     ;; the current major mode for the buffer.
     "["
 
-    '(:eval (propertize "%m" 'face nil
+    '(:eval (propertize "%m" 'face 'font-lock-string-face
               'help-echo buffer-file-coding-system))
     " "
 
 
     ;; insert vs overwrite mode, input-method in a tooltip
     '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-              'face nil
+              'face 'font-lock-preprocessor-face
               'help-echo (concat "Buffer is in "
                            (if overwrite-mode "overwrite" "insert") " mode")))
 
     ;; was this buffer modified since the last save?
     '(:eval (when (buffer-modified-p)
-              (concat ","  (propertize "Mod"
-                             'face nil
+              (concat "," (propertize "Mod"
+                             'face 'font-lock-warning-face
                              'help-echo "Buffer has been modified"))))
 
     ;; is this buffer read-only?
     '(:eval (when buffer-read-only
-              (concat ","  (propertize "RO"
-                             'face nil
+              (concat "," (propertize "RO"
+                             'face 'font-lock-type-face
                              'help-echo "Buffer is read-only"))))
     "] "
 
     '(-3 . "%P")
     " "
     ;;global-mode-string, org-timer-set-timer in org-mode need this
-    (propertize "%M" 'face nil)
+    (propertize "%M" 'face 'font-lock-type-face)
 
     " --"
     ;; i don't want to see minor-modes; but if you want, uncomment this:
-    ;; minor-mode-alist  ;; list of minor modes
+    ;; minor-mode-alist ;; list of minor modes
     "%-" ;; fill with '-'
     ))
 
 (provide 'init-modeline)
-
