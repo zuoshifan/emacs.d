db21ee1a7915b74dce847b5ceac610d63ec9f796
Chen Bin <chenbin.sh@gmail.com>
2014-02-19 11:30:00 +1100

    remove slime in elpa since we already lock it

--- init-elpa.el
+++ init-elpa.el
@@ -161,7 +161,6 @@ re-downloaded in order to locate PACKAGE."
 (require-package 'paredit)
 (require-package 'eldoc-eval)
 (require-package 'erlang '(20120612 0 0) nil)
-(require-package 'slime)
 (require-package 'browse-kill-ring)
 (require-package 'findr)
 (require-package 'jump '(2 3 0) nil)
