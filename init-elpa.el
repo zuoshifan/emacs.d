1a682ff13f9d39c1fc420e5b37f98de8169aecdf
Chen Bin <chenbin.sh@gmail.com>
2014-05-14 17:06:17 +1000

    use company-0.8.0 from elpa
    Conflicts:
    
    	init-elpa.el

--- init-elpa.el
+++ init-elpa.el
@@ -276,7 +276,8 @@ re-downloaded in order to locate PACKAGE."
 (require-package 'sr-speedbar)
 (require-package 'requirejs-mode)
 (require-package 'smartparens)
-(require-package 'company '(0 7 0) nil)
+;; company-mode drop emacs 23 support
+(if (and (>= emacs-major-version 24)) (require-package 'company '(0 8 0) nil))
 (require-package 'legalese)
 (require-package 'string-edit)
 (require-package 'dired-details)
