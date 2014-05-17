70755ef9a195f12c887cf9f03347c912803b0556
Chen Bin <chenbin.sh@gmail.com>
2014-05-17 11:21:01 +1000

    README on emacs fetching packages when offline

--- init-elpa.el
+++ init-elpa.el
@@ -86,6 +86,12 @@ re-downloaded in order to locate PACKAGE."
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
+;; Un-comment below line if you download zip file from https://github.com/redguardtoo/myelpa/archive/master.zip and extract its content into ~/myelpa/
+;; (setq package-archives '(("myelpa" . "~/myelpa/")))
+
+;; Or Un-comment below line if you prefer installing package from https://github.com/redguardtoo/myelpa/ directly
+;; (setq package-archives '(("myelpa" . "https://raw.github.com/redguardtoo/myelpa/master/")))
+
 (defvar melpa-include-packages
   '(bbdb
     lua-mode
