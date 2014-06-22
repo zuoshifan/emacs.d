4d9940a7ad3ed7dc9d7ee10e59998b9a10237747
zuoshifan <zuoshifan@163.com>
2014-06-22 09:28:57 +0800

    Resolve cdlatex and yasnippet TAB conflict.

--- init-cdlatex.el
+++ init-cdlatex.el
@@ -11,4 +11,21 @@
 (add-hook 'LaTeX-mode-hook 'turn-on-cdlatex)   ; with AUCTeX LaTeX mode
 (add-hook 'latex-mode-hook 'turn-on-cdlatex)   ; with Emacs latex mode
 
+;; when cdlatex-mode or org-cdlatex-mode are loaded, we need to change
+;; the behaviour of yas/fallback to call cdlatex-tab
+;; See http://spw.sdf.org/oldnotes/2010/12/making-yasnippet-and-cdlatex-mode-play-nice-together.html
+(defun yas/advise-indent-function (function-symbol)
+  (eval `(defadvice ,function-symbol (around yas/try-expand-first activate)
+           ,(format
+             "Try to expand a snippet before point, then call `%s' as usual"
+             function-symbol)
+           (let ((yas/fallback-behavior nil))
+             (unless (and (interactive-p)
+                          (yas/expand))
+               ad-do-it)))))
+
+(yas/advise-indent-function 'cdlatex-tab)
+(yas/advise-indent-function 'org-cycle)
+(yas/advise-indent-function 'org-try-cdlatex-tab)
+
 (provide 'init-cdlatex)
