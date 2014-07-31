c4bcf9fd7197f8fdbc4f542d5d3c882b1254143b
zuoshifan <zuoshifan@163.com>
2014-07-31 09:02:53 +0800

    Ace-jump-mode key-binding.

--- init-ace-jump-mode.el
+++ init-ace-jump-mode.el
@@ -7,7 +7,7 @@
   "Emacs quick move minor mode"
   t)
 ;; you can select the key you prefer to
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode)
 (define-key global-map (kbd "C-c j j") 'ace-jump-buffer)
 
 ;; 
