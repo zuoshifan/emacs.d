5d0c061581635eab8532fcd5ee6956bee35b8d10
Chen Bin <chenbin.sh@gmail.com>
2014-08-19 15:12:12 +1000

    make color-theme support daemon mode
    Conflicts:
    
    	init.el

--- init.el
+++ init.el
@@ -160,10 +160,16 @@
 
 
 
-;; color theme
 (require 'color-theme)
 ;; (require 'color-theme-molokai)
-;; (color-theme-molokai)
+;; color theme
+;; (if (daemonp) ;; if run from daemon
+;;   (add-hook 'after-make-frame-functions
+;;             (lambda (frame)
+;;               (select-frame frame)
+;;               (color-theme-molokai)))
+;;   (color-theme-molokai))
+
 ;; misc has some crucial tools I need immediately
 (require 'init-misc)
 
