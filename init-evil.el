6887b72c0bb7dd92bc42131013ca98b4e4871cac
Chen Bin <chenbin.sh@gmail.com>
2014-08-22 00:58:23 +1000

    rinari setup

--- init-evil.el
+++ init-evil.el
@@ -265,7 +265,7 @@ to replace the symbol under cursor"
   "rnk" 'rinari-rake
   "rnm" 'rinari-find-model
   "rnl" 'rinari-find-log
-  "rns" 'rinari-console
+  "rno" 'rinari-console
   "rnt" 'rinari-find-test
   "rbd" 'robe-doc
   "rbj" 'robe-jump
@@ -317,7 +317,7 @@ to replace the symbol under cursor"
   "xb" 'ido-switch-buffer
   "xc" 'save-buffers-kill-terminal
   "xo" 'helm-find-files
-  "ri" 'yari-helm
+  "ri" '(lambda () (interactive) (require 'helm) (yari-helm))
   "vv" 'scroll-other-window
   "vu" '(lambda () (interactive) (scroll-other-window '-))
   "vr" 'vr/replace
