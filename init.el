97634639236dfa6a15fd4c95a5b9e990b14af147
zuoshifan <zuoshifan@163.com>
2014-09-24 19:23:31 +0800

    Add init-benchmarking.

--- init.el
+++ init.el
@@ -1,34 +1,44 @@
 ;; -*- coding: utf-8 -*-
+
+;;; This file bootstraps the configuration, which is divided into
+;;; a number of other files.
+
+(let ((minver 23))
+  (unless (>= emacs-major-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+
+
+(add-to-list 'load-path (expand-file-name user-emacs-directory))
+(require 'init-benchmarking) ;; Measure startup time
 (setq emacs-load-start-time (current-time))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 
 ;;----------------------------------------------------------------------------
 ;; Which functionality to enable (use t or nil for true and false)
 ;;----------------------------------------------------------------------------
-(setq *macbook-pro-support-enabled* t)
-(setq *is-a-mac* (eq system-type 'darwin))
-(setq *is-carbon-emacs* (and *is-a-mac* (eq window-system 'mac)))
-(setq *is-cocoa-emacs* (and *is-a-mac* (eq window-system 'ns)))
-(setq *win32* (eq system-type 'windows-nt) )
-(setq *cygwin* (eq system-type 'cygwin) )
-(setq *linux* (or (eq system-type 'gnu/linux) (eq system-type 'linux)) )
-(setq *unix* (or *linux* (eq system-type 'usg-unix-v) (eq system-type 'berkeley-unix)) )
-(setq *linux-x* (and window-system *linux*) )
-(setq *xemacs* (featurep 'xemacs) )
-(setq *emacs23* (and (not *xemacs*) (or (>= emacs-major-version 23))) )
-(setq *emacs24* (and (not *xemacs*) (or (>= emacs-major-version 24))) )
-(setq *no-memory* (cond
-                   (*is-a-mac*
-                    (< (string-to-number (nth 1 (split-string (shell-command-to-string "sysctl hw.physmem")))) 4000000000))
-                   (*linux* nil)
-                   (t nil)
-                   ))
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
+(defconst *macbook-pro-support-enabled* t)
+(defconst *is-carbon-emacs* (and *is-a-mac* (eq window-system 'mac)))
+(defconst *is-cocoa-emacs* (and *is-a-mac* (eq window-system 'ns)))
+(defconst *win32* (eq system-type 'windows-nt))
+(defconst *cygwin* (eq system-type 'cygwin))
+(defconst *linux* (or (eq system-type 'gnu/linux) (eq system-type 'linux)))
+(defconst *unix* (or *linux* (eq system-type 'usg-unix-v) (eq system-type 'berkeley-unix)))
+(defconst *linux-x* (and window-system *linux*))
+(defconst *xemacs* (featurep 'xemacs) )
+(defconst *emacs23* (and (not *xemacs*) (or (>= emacs-major-version 23))))
+(defconst *emacs24* (and (not *xemacs*) (or (>= emacs-major-version 24))))
+(defconst *no-memory* (cond
+                       (*is-a-mac*
+                        (< (string-to-number (nth 1 (split-string (shell-command-to-string "sysctl hw.physmem")))) 4000000000))
+                       (*linux* nil)
+                       (t nil)))
 
 ;----------------------------------------------------------------------------
 ; Functions (load all files in defuns-dir)
 ; Copied from https://github.com/magnars/.emacs.d/blob/master/init.el
 ;----------------------------------------------------------------------------
-(setq defuns-dir (expand-file-name "~/.emacs.d/defuns"))
+(setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
 (dolist (file (directory-files defuns-dir t "\\w+"))
   (when (file-regular-p file)
       (load file)))
@@ -99,6 +109,7 @@
 (require 'init-python-mode)
 ;; (require 'init-haskell) ;; idle require
 ;; (require 'init-ruby-mode) ;; idle require
+;; (require 'init-paredit)
 (require 'init-elisp)
 (require 'init-marmalade)
 
