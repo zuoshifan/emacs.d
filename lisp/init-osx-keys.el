2dbd1cd896193872c74bd7a43c184026d5571e20
zuoshifan <zuoshifan@163.com>
2014-09-27 19:43:38 +0800

    Synchronize with Purcell for init-osx-keys.el.

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -3,14 +3,18 @@
   (setq mac-option-modifier 'none)
   (setq default-input-method "MacOSX")
   ;; Make mouse wheel / trackpad scrolling less jerky
-  (setq mouse-wheel-scroll-amount '(0.001))
-  (when *is-cocoa-emacs*
-    ;; Woohoo!!
-    (global-set-key (kbd "M-`") 'ns-next-frame)
-    (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
-    (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports
-    (global-set-key (kbd "M-c") 'ns-copy-including-secondary)
-    (global-set-key (kbd "M-v") 'ns-paste-secondary)))
+  (setq mouse-wheel-scroll-amount '(1
+                                    ((shift) . 5)
+                                    ((control))))
+  (dolist (multiple '("" "double-" "triple-"))
+    (dolist (direction '("right" "left"))
+      (global-set-key (kbd (concat "<" multiple "wheel-" direction ">")) 'ignore)))
+  (global-set-key (kbd "M-`") 'ns-next-frame)
+  (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
+  (after-load 'nxml-mode
+    (define-key nxml-mode-map (kbd "M-h") nil))
+  (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
+  )
 
 
 (provide 'init-osx-keys)
