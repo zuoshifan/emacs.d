ea1061d9743ee3903e49267a758660998c23d48b
zuoshifan <zuoshifan@163.com>
2014-10-02 22:20:01 +0800

    Partly synchronize with Purcell for init-ido.el.

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -1,15 +1,31 @@
 ;; Use C-f during file selection to switch to regular find-file
-(require 'ido-ubiquitous)
+(require 'ido)
 (ido-mode t)  ; use 'buffer rather than t to use only buffer switching
 (ido-everywhere t)
-;(ido-ubiquitous-mode t)
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point nil)
 (setq ido-auto-merge-work-directories-length 0)
 (setq ido-use-virtual-buffers t)
 
+(when (eval-when-compile (>= emacs-major-version 24))
+ (require-package 'ido-ubiquitous)
+ (ido-ubiquitous-mode t))
+
+;; Use smex to handle M-x
+(when (eval-when-compile (>= emacs-major-version 24))
+  (require-package 'smex)
+  ;; Change path for ~/.smex-items
+  (setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+  (global-set-key [remap execute-extended-command] 'smex))
+
+(require-package 'idomenu)
+
 ;; Allow the same buffer to be open in different frames
 (setq ido-default-buffer-method 'selected-window)
+
+;; http://www.reddit.com/r/emacs/comments/21a4p9/use_recentf_and_ido_together/cgbprem
+(add-hook 'ido-setup-hook (lambda () (define-key ido-completion-map [up] 'previous-history-element)))
+
 
 (defun steve-ido-choose-from-recentf ()
   "Use ido to select a recently opened file from the `recentf-list'"
