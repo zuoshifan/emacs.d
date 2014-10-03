dbc09ee4ae6bee4b8e8be9b080479b0f399db05a
zuoshifan <zuoshifan@163.com>
2014-10-03 21:24:03 +0800

    Synchronize with Purcell for init-csv.el.

--- lisp/init-csv.el
+++ lisp/init-csv.el
@@ -1,3 +1,6 @@
+(require-package 'csv-mode)
+(require-package 'csv-nav)
+
 (autoload 'csv-mode "csv-mode" "Major mode for editing comma-separated value files." t)
 (add-auto-mode 'csv-mode "\\.[Cc][Ss][Vv]\\'")
 (autoload 'csv-nav-mode "csv-nav-mode" "Major mode for navigating comma-separated value files." t)
