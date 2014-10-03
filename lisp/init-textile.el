6ace0bc04e726acd00bb90ab81a59bd021ace73e
zuoshifan <zuoshifan@163.com>
2014-10-03 21:23:12 +0800

    Synchronize with Purcell for init-textile.el.

--- lisp/init-textile.el
+++ lisp/init-textile.el
@@ -1,5 +1,9 @@
+(require-package 'textile-mode)
+
 (autoload 'textile-mode "textile-mode" "Mode for editing Textile documents" t)
 (setq auto-mode-alist
       (cons '("\\.textile\\'" . textile-mode) auto-mode-alist))
 
+
 (provide 'init-textile)
+
