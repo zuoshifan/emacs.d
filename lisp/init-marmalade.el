e76c61e02ccac2c039648b7b45d6ba4396d4a254
zuoshifan <zuoshifan@163.com>
2014-10-04 21:58:30 +0800

    Synchronize with Purcell for init-marmalade.el.

--- lisp/init-marmalade.el
+++ lisp/init-marmalade.el
@@ -1,9 +1,18 @@
+(require-package 'marmalade)
+
 ;;; Handy code for uploading new versions of my own packages to marmalade
 
 (autoload 'marmalade-upload-buffer "marmalade")
 
+(defun sanityinc/parse-git-version (s)
+  "Return numeric version array parsed from S, or nil."
+  (ignore-errors (version-to-list s)))
+
 (defun latest-version-from-git-tag ()
-  (let ((versions (mapcar #'version-to-list (split-string (shell-command-to-string "git tag")))))
+  (let ((versions
+         (remove-if #'null
+                    (mapcar #'sanityinc/parse-git-version
+                            (split-string (shell-command-to-string "git tag"))))))
     (sort versions #'version-list-<)
     (package-version-join (car (last versions)))))
 
@@ -40,13 +49,12 @@
   "Submit the elisp library in BUF to Marmalade."
   (interactive
    (list
-    (completing-read "Submit buffer: "
-                     (mapcar 'buffer-name (buffer-list))
-                     (lambda (b)
-                       (with-current-buffer b
-                         (and buffer-file-name
-                              (eq major-mode 'emacs-lisp-mode))))
-                     t)))
+    (let ((buffers (loop for b in (mapcar 'buffer-name (buffer-list))
+                         when (with-current-buffer b
+                                (and buffer-file-name
+                                     (eq major-mode 'emacs-lisp-mode)))
+                         collect b)))
+      (completing-read "Submit buffer: " buffers nil t nil nil (car buffers)))))
   (with-current-buffer buf
     (let ((tag (latest-version-from-git-tag)))
       (unless tag
