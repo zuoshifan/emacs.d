95e2bcea370efb07d6bd4caec02313367c266b0b
zuoshifan <zuoshifan@163.com>
2014-11-09 20:10:16 +0800

    New melpa url.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -84,6 +84,7 @@ re-downloaded in order to locate PACKAGE."
 
 ;;; Also use Melpa for most packages
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
 
 
