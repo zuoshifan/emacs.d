568f5ad0a58c7f8bc39172d57981546e4b8790fa
zuoshifan <zuoshifan@163.com>
2014-11-09 20:09:37 +0800

    Change in flycheck.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -6,7 +6,7 @@
 (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
       flycheck-idle-change-delay 0.8)
 
-(setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+;; (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
 
 
 (provide 'init-flycheck)
