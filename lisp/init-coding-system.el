95917b6eec5cde9e569b08f6400433fc03289ace
zuoshifan <zuoshifan@163.com>
2014-12-16 22:28:55 +0800

    Chinese font set

--- lisp/init-coding-system.el
+++ lisp/init-coding-system.el
@@ -2,4 +2,7 @@
 (prefer-coding-system 'chinese-gbk)
 (prefer-coding-system 'utf-8)
 
+;; Chinese font set
+(set-fontset-font t 'han (font-spec :family "WenQuanYi Micro Hei Mono"))
+
 (provide 'init-coding-system)
