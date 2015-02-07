a7201bdb6b3a87305f7b6ab7b9a9f921f4827bad
Steve Purcell <steve@sanityinc.com>
2015-02-07 12:30:23 +0000

    Suppress symbol highlighting while isearch is active

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -110,10 +110,12 @@
 (require-package 'highlight-symbol)
 (dolist (hook '(prog-mode-hook html-mode-hook css-mode-hook))
   (add-hook hook 'highlight-symbol-mode)
-  ;; (add-hook hook 'highlight-symbol-nav-mode)
-  )
-(eval-after-load 'highlight-symbol
-  '(diminish 'highlight-symbol-mode))
+  (add-hook hook 'highlight-symbol-nav-mode))
+(after-load 'highlight-symbol
+  (diminish 'highlight-symbol-mode)
+  (defadvice highlight-symbol-temp-highlight (around sanityinc/maybe-suppress activate)
+    "Suppress symbol highlighting while isearching."
+    (unless isearch-mode ad-do-it)))
 
 ;;----------------------------------------------------------------------------
 ;; Zap *up* to char is a more sensible default
