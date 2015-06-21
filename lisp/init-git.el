c3b693d90f510cd232833843440bc0940d00b771
zuoshifan <zuoshifan@163.com>
2015-06-21 18:49:30 -0400

    Some changes in init-git

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -63,14 +63,16 @@
 
 (add-hook 'git-commit-mode-hook 'goto-address-mode)
 (after-load 'session
-  (add-to-list 'session-mode-disable-list 'git-commit-mode))
+  (when (boundp 'session-mode-disable-list) ; newer Emacsen
+    (add-to-list 'session-mode-disable-list 'git-commit-mode)))
 
 
 ;;; When we start working on git-backed files, use git-wip if available
 
 (after-load 'magit
-  (global-magit-wip-save-mode)
-  (diminish 'magit-wip-save-mode))
+  (when (executable-find magit-git-executable)
+    (global-magit-wip-save-mode)
+    (diminish 'magit-wip-save-mode)))
 
 ;; (after-load 'magit
 ;;   (diminish 'magit-auto-revert-mode))
@@ -103,19 +105,20 @@
     (add-to-list 'compilation-error-regexp-alist (car defn))))
 
 (defvar git-svn--available-commands nil "Cached list of git svn subcommands")
+(defun git-svn--available-commands ()
+  (or git-svn--available-commands
+      (setq git-svn--available-commands
+            (sanityinc/string-all-matches
+             "^  \\([a-z\\-]+\\) +"
+             (shell-command-to-string "git svn help") 1))))
 
-(defun git-svn (dir)
+(defun git-svn (dir command)
   "Run a git svn subcommand in DIR."
-  (interactive "DSelect directory: ")
-  (unless git-svn--available-commands
-    (setq git-svn--available-commands
-          (sanityinc/string-all-matches
-           "^  \\([a-z\\-]+\\) +"
-           (shell-command-to-string "git svn help") 1)))
+  (interactive (list (read-directory-name "Directory: ")
+                     (completing-read "git-svn command: " (git-svn--available-commands) nil t nil nil (git-svn--available-commands))))
   (let* ((default-directory (vc-git-root dir))
          (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
-    (compile (concat "git svn "
-                     (ido-completing-read "git-svn command: " git-svn--available-commands nil t)))))
+    (compile (concat "git svn " command))))
 
 
 (require-package 'git-messenger)
