885cef6af1a8ce48c87df7ca5a17e34addb14265
zuoshifan <zuoshifan@163.com>
2015-06-21 18:17:45 -0400

    Key map change in init-osx-keys

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -1,6 +1,6 @@
 (when *is-a-mac*
   (setq mac-command-modifier 'meta)
-  (setq mac-option-modifier 'none)
+  (setq mac-option-modifier 'control)
   (setq default-input-method "MacOSX")
   ;; Make mouse wheel / trackpad scrolling less jerky
   (setq mouse-wheel-scroll-amount '(1
